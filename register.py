d7be2cef861aa7d1eba9136e17dfee90b853a209
priyathadakamalla49-collab <priyathadakamalla49@gmail.com>
2026-02-11 16:08:03 +0530

    Add files via upload

---
+++ register.py
@@ -0,0 +1,18 @@
+# Python task :  
+
+
+
+#register a user  with below values
+#name 
+#age
+#mobile
+#email -- optional
+#city --  optional
+#dob-- optional
+
+
+def register(name,age,mobile,email = "a@gmail.com",city = "Hyd", DOB = "2000-02-01"):
+    print(f"name: {name},age: {age},mobile : {mobile},email:{email},city:{city},DOB: {DOB}")
+
+register("Yash",mobile = 98766544556,age = 22,city = 'Bangalore')
+    
